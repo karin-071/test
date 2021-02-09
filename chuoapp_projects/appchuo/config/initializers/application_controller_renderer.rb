b7ed8e9ae326afe52f1b6b653d3e8b575aae5479
yutaro824 <yamakawa0824@gmail.com>
2021-02-09 10:41:01 +0000

    chuoapp commit

---
+++ chuoapp_projects/appchuo/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,11 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+
+Refile.secret_key = '32947e360373e36fdbad1a8c32e5cf366336a0ace5e61e08f85a9775548a956750b8e58a905c3d5045dcae59a604c8ad27692cc2671c59b88c5a8da814e665ba'
